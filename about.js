99b6a17c49617d7eff33a502d4b03c3ab0c667a8
Garrett Pace <elderpace6@gmail.com>
2023-04-28 11:59:49 -0700

    Part4

--- about.js
+++ about.js
@@ -1,27 +1,15 @@
 console.log("hello world");
 
-
-
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
-	alert('Your form has been submitted!');
+		alert('Your form has been submitted!');
 }
-
-
 let form = document.querySelector('#contact');
-
 form.addEventListener('submit', handleSubmit);
-
-
 
 function compliment(evt1) {
 	evt1.preventDefault();
-	
-	alert('You ARE the tiger');
+		alert('You ARE the tiger');
 }
-
-
 let form1 = document.querySelector('img');
-
 form1.addEventListener('mouseover', compliment);
---
+++ profile.js
@@ -0,0 +1,22 @@
+console.log('Hello World')
+
+let form2 = document.querySelector('color')
+let form3 = document.querySelector('place')
+let form4 = document.querySelector('ritual')
+
+
+form2.addEventListener('click', (event) => {
+    event.preventDefault()
+    alert('My Favorite Color is RED')
+})
+
+form3.addEventListener('click', (event) => {
+    event.preventDefault()
+    alert('My Favorite Place is Mexico')
+})
+
+form4.addEventListener('click', (event) => {
+    event.preventDefault()
+    alert('My Favorite Ritual is Mexico')
+})
+
