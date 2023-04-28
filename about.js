10420245c13c38b0d72d8c063090b1a2f447720c
Garrett Pace <elderpace6@gmail.com>
2023-04-28 10:14:32 -0700

    Part2

--- about.js
+++ about.js
@@ -5,10 +5,23 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Your form has been submitted!');
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+
+
+function compliment(evt1) {
+	evt1.preventDefault();
+	
+	alert('You ARE the tiger');
+}
+
+
+let form1 = document.querySelector('img');
+
+form1.addEventListener('mouseover', compliment);
